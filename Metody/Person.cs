404c9892df22fd68ad5fb5987a92a2fdfda86653
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-03-09 11:53:23 +0100

    Add project files.

---
+++ Metody/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metody
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        private DateTime dateOfBirth;
+
+        public Person(string firstName, string lastName)
+        {
+            Console.WriteLine("Constructor1");
+            FirstName = firstName;
+            LastName = lastName;
+                
+        }
+        public Person(DateTime dateOfBirth, string firstName, string lastName) : this(firstName, lastName)
+        {
+            Console.WriteLine("Constructor2");
+
+            SetDateOfBirth(dateOfBirth);
+
+        }
+
+        public void SetDateOfBirth(DateTime date)
+        {
+            if(date > DateTime.Now)
+            {
+                Console.WriteLine("Invalid date of birth");
+            }
+            else
+            {
+                dateOfBirth = date;
+            }
+
+        }
+        public DateTime GetDateOfBirth() => dateOfBirth;
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {FirstName}, {GetDateOfBirth()}");
+        }
+        
+    }
+}
---
+++ Metody/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Metody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bill = new Person("Bill", "Wick");
+
+            bill.FirstName = "Bill";
+
+
+            bill.SetDateOfBirth(new DateTime(1990, 1, 1));
+
+            //Console.WriteLine($"{bill.FirstName} {bill.GetDateOfBirth()}");
+
+            bill.SayHi();
+
+            Person john = new Person(new DateTime(1990, 1, 2), "John", "Wick");
+
+            john.SayHi();
+        }
+    }
+}
